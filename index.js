75d84373bce39d02c26af33c441067810c1b1b00
robertocaamanor <raristides.caamano@gmail.com>
2021-12-29 21:41:49 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,81 @@
+var express = require('express')
+const data = require("./model/videos"); //llamamos a Express
+var app = express();
+const bodyParser = require('body-parser'),    
+      jwt = require('jsonwebtoken'),
+      config = require('./configs/config');
+
+
+//enable CORS for request verbs
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-token");
+    res.header("Access-Control-Allow-Methods","POST, GET, PUT, DELETE, OPTIONS");
+    res.header("Cache-Control: no-cache, no-store, must-revalidate");
+    res.header("Pragma: no-cache");
+    next();
+});
+
+app.set('llave', config.llave);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.post('/autenticar', (req, res) => {
+    if(req.body.usuario === "asfo" && req.body.contrasena === "holamundo") {
+  const payload = {
+   check:  true
+  };
+  const token = jwt.sign(payload, app.get('llave'), {
+   expiresIn: 1440
+  });
+  res.json({
+   mensaje: 'Autenticación correcta',
+   token: token
+  });
+    } else {
+        res.json({ mensaje: "Usuario o contraseña incorrectos"})
+    }
+})
+
+const rutasProtegidas = express.Router(); 
+rutasProtegidas.use((req, res, next) => {
+    const token = req.headers['access-token'];
+ 
+    if (token) {
+      jwt.verify(token, app.get('llave'), (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Token inválida' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token no proveída.' 
+      });
+    }
+ });
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/videos', rutasProtegidas, function(req, res) {
+    const filters = req.query;
+    const filteredUsers = data.filter(user => {
+        let isValid = true;
+        for (key in filters) {
+            console.log(key, user[key], filters[key]);
+            isValid = isValid && user[key] == filters[key];
+        }
+        return isValid;
+    });
+    res.send(filteredUsers);
+
+})
+var port = process.env.PORT || 8080  // establecemos nuestro puerto
+
+// iniciamos nuestro servidor
+app.listen(port)
+console.log('API escuchando en el puerto ' + port)
---
+++ model/videos.js
@@ -0,0 +1,28 @@
+const listaVideos = [
+    {
+        "id": 1,
+        "title": "Tandas Comerciales Canal 13 - Enero 1989",
+        "user": "Archivos en VHS",
+        "reproducciones": "1030",
+        "image": "tanda_canal13_enero1989.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis rutrum maximus. Duis rutrum ornare tempus. Donec ullamcorper nisl in mi imperdiet tempor. Sed tempor lorem sit amet tortor tristique posuere. Donec ut quam eget ante hendrerit semper. Nullam lorem nisi, interdum ac mattis a, consectetur eu massa. Praesent felis justo, suscipit non tortor non, condimentum molestie massa. Curabitur placerat metus eros, id eleifend urna condimentum nec."
+    },
+    {
+        "id": 2,
+        "title": "Tandas Comerciales Megavisión - Mayo 1995",
+        "user": "Archivos en VHS",
+        "reproducciones": "1030",
+        "image": "tanda_megavision_noviembre1995.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis rutrum maximus. Duis rutrum ornare tempus. Donec ullamcorper nisl in mi imperdiet tempor. Sed tempor lorem sit amet tortor tristique posuere. Donec ut quam eget ante hendrerit semper. Nullam lorem nisi, interdum ac mattis a, consectetur eu massa. Praesent felis justo, suscipit non tortor non, condimentum molestie massa. Curabitur placerat metus eros, id eleifend urna condimentum nec."
+    },
+    {
+        "id": 3,
+        "title": "Katy Perry - Rock in Rio 2015",
+        "user": "Telearchivos Retro",
+        "reproducciones": "1030",
+        "image": "katyperry_rockinrio_2015.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis rutrum maximus. Duis rutrum ornare tempus. Donec ullamcorper nisl in mi imperdiet tempor. Sed tempor lorem sit amet tortor tristique posuere. Donec ut quam eget ante hendrerit semper. Nullam lorem nisi, interdum ac mattis a, consectetur eu massa. Praesent felis justo, suscipit non tortor non, condimentum molestie massa. Curabitur placerat metus eros, id eleifend urna condimentum nec."
+    }
+]
+
+module.exports = listaVideos;
